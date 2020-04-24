1f47b72867990214c10285da2c8fe86adf5ceb1e
Priyanka Dadlani <dadlani.priyanka@gmail.com>
2020-04-24 09:28:13 -0700

    finished ring buffer.

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -8,14 +8,44 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        # if the buffer is at capacity
+        if self.storage.length == self.capacity:
+            # if current is at the tail, that means the item must be appended to the head
+            if self.current == self.storage.tail:
+                # remove the current head
+                self.storage.remove_from_head()
+                # add item to head
+                self.storage.add_to_head(item)
+                # set current equal to the new head
+                self.current = self.storage.head
+            # else, current is somewhere other than the tail
+            else:
+                # insert item just after current
+                self.current.insert_after(item)
+                # update length because insert_after does not
+                self.storage.length += 1
+                # update current
+                self.current = self.current.next
+                # delete item that is just after current aka the oldest item
+                self.storage.delete(self.current.next)
+        # otherwise, if the buffer is not at capacity, aka below capacity
+        else:
+            # add item to tail of buffer
+            self.storage.add_to_tail(item)
+            # update current to point to tail
+            self.current = self.storage.tail
     
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
-
-        # TODO: Your code here
-
+        # create a variable - item - to start by pointing at head
+        item = self.storage.head
+        # while item is not None
+        while item is not None:
+            # append item to the list buffer content provided
+            list_buffer_contents.append(item.value)
+            # update item to the next in list
+            item = item.next
         return list_buffer_contents
 
 # ----------------Stretch Goal-------------------
@@ -30,5 +60,6 @@ class ArrayRingBuffer:
         
 
     def get(self):
+        # use a list comprehension to add each item from the ring buffer
         pass
     
